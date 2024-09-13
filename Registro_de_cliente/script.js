b61946ba4e39df27a5b0f6dd8565b151cb0b8b46
AntonioMedranda <antoniomedranda646@gmail.com>
2024-09-13 08:55:31 -0500

    Add files via upload
    Tarea 3
    Formulario de clientes

---
+++ Registro_de_cliente/script.js
@@ -0,0 +1,8 @@
+
+// El formulario se envía sin validaciones adicionales
+document.getElementById('clienteForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que la página se recargue
+    alert('Datos enviados correctamente.'); // Simple alerta de confirmación
+});
+
+
